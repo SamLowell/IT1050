fe245a9279a9443af18449ad1ae813ef531ca892
samwisesail@gmail.com <samwisesail@gmail.com>
2019-01-24 20:53:26 -0500

    Beggining choose your own adventure.

---
+++ WoodsCYOA/WoodsCYOA/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoodsCYOA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  //short story using Console.Write and Console.Read
+            Console.WriteLine("You are walking on a path in the woods.");
+            System.Console.ReadKey();
+            Console.WriteLine("Ahead of you there is a dog and a cat.");
+            System.Console.ReadKey();
+            Console.Write("What is the dog's name? ");
+            string dogName = Console.ReadLine();
+            Console.Write("What is the cat's name? ");
+            string catName = Console.ReadLine();
+
+            Console.WriteLine(dogName + " and " + catName + " head in opposite directions.");
+            //Reader chooses animal to follow
+            System.Console.ReadKey();
+            Console.WriteLine("Will you follow:");
+            Console.WriteLine(dogName + "?");
+            Console.WriteLine(catName + "?");
+
+            System.Console.ReadKey();
+        }
+    }
+}
