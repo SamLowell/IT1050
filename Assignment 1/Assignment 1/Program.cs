75d640ac7dba5337e8faf5178bab2b0992212d4f
samwisesail@gmail.com <samwisesail@gmail.com>
2019-02-06 19:50:57 -0500

    Final changes

--- Assignment 1/Assignment 1/Program.cs
+++ Assignment 1/Assignment 1/Program.cs
@@ -13,14 +13,7 @@ namespace Assignment_1
         {
             string firstName;
             string middleInitail;
-            string lastName;
-            int age;
-            bool isCitizen;
-            bool canVote;
-            int heightFeet;
-            double heightInches;
-            double totalHeightCM;
-
+            string lastName;                 
             Console.Write("What is your first name? ");
             firstName = Console.ReadLine();
             Console.Write("What is your middle initial? ");
@@ -28,19 +21,29 @@ namespace Assignment_1
             Console.Write("What is your last name? ");
             lastName = Console.ReadLine();
 
+            int heightFeet;
+            double heightInches;
+            double totalHeightCM;
+            double Inch_to_Cm = 2.54;
             Console.Write("How many feet tall are you? ");
             heightFeet = int.Parse(Console.ReadLine());
             Console.Write("How many remaining inches tall are you? ");
             heightInches = double.Parse(Console.ReadLine());
-            totalHeightCM = (heightInches + heightFeet * 12) * 2.54;
-                    
+            int inchInFoot = 12;
+            int feet_to_inch = heightFeet * inchInFoot;
+            double totalInch = feet_to_inch + heightInches;
+            totalHeightCM = totalInch * Inch_to_Cm;
+
+            int age;
+            bool canVote;
             Console.Write("How old are you? ");
             age = int.Parse(Console.ReadLine());
             Console.Write("Are you a US citizen? ");
-            isCitizenstring = Console.ReadLine().ToLower();
-            bool isCitizen = isCitizenstring.Contains("y") || isCitizenString.Contains("t");
+            string isCitizenstring = Console.ReadLine().ToLower();
+            bool isCitizen = isCitizenstring.Contains("y") || isCitizenstring.Contains("t");
             canVote = age >= 18 && isCitizen;
 
+            Console.WriteLine("");
             Console.WriteLine("Name: " + firstName + " " + middleInitail + ". " + lastName);
             Console.WriteLine("Height: " + totalHeightCM + "cm");
             Console.WriteLine("Able to vote: " + canVote);
