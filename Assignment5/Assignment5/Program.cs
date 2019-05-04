144075696484908511b4ba41d90944f471648410
samwisesail@gmail.com <samwisesail@gmail.com>
2019-05-04 15:18:47 -0400

    Started Assignment 5 and Final

---
+++ Assignment5/Assignment5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            void PrintBoard()
+            {
+
+
+                for (int i = 0; i < 8; i++)
+                {
+                    Console.Write("X");
+                }
+            }
+
+            PrintBoard();
+            
+
+
+        }
+    }
+}
