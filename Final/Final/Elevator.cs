60bafc5126680d8daa90f84adb39f933891b314f
samwisesail@gmail.com <samwisesail@gmail.com>
2019-05-04 17:21:46 -0400

    Completed Final

---
+++ Final/Final/Elevator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final
+{
+    class Elevator
+    {
+        public double MaxWeight { get; set; }
+        private Passenger[] Occupants;
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            MaxWeight = maxWeight;
+            Occupants = new Passenger[maxOccupants];
+        }
+
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            Occupants[index] = passenger;
+        }
+
+        public double GetCurrentWeight()
+        {
+            double CurrentWeight = 0;
+
+            foreach(Passenger passenger in Occupants)
+            {
+                CurrentWeight += passenger.GetWeight();
+            } 
+                
+            return CurrentWeight;
+        }
+
+        public bool IsOverMaxCapacity()
+        {
+            return this.GetCurrentWeight() > this.MaxWeight;
+
+        }
+
+    }
+}
---
+++ Final/Final/Passenger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final
+{
+    class Passenger
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+
+        public Passenger (string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public double GetWeight()
+        {
+            return Weight;
+        }
+
+
+
+    }
+}
