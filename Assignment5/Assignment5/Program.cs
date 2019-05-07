bcad490386864619519f58d9e13e427667fc85a5
samwisesail@gmail.com <samwisesail@gmail.com>
2019-05-07 19:14:51 -0400

    able to move one piece

--- Assignment5/Assignment5/Program.cs
+++ Assignment5/Assignment5/Program.cs
@@ -8,25 +8,91 @@ namespace Assignment5
 {
     class Program
     {
+        public const int SIZE = 8;
+        public static string[][] board;
+
         static void Main(string[] args)
         {
-
-
-
-            void PrintBoard()
-            {
-
-
-                for (int i = 0; i < 8; i++)
-                {
-                    Console.Write("X");
-                }
-            }
-
+            CreateBoard();
             PrintBoard();
+            movePiece();
+            Console.ReadKey();
+           
             
 
 
-        }
+            void CreateBoard()
+            {
+                Program.board = new string[SIZE][];
+                for (int i = 0; i < SIZE; i++)
+                {
+                    board[i] = new string[SIZE];
+                    for (int j = 0; j < SIZE; j++)
+                    {
+                        board[i][j] = (i < 2 || i > 5) ? "X" : " ";
+                    }
+                }
+            }
+
+            void printDivider()
+            {
+                Console.WriteLine("----------------------------------");
+            }
+
+            void PrintBoard()
+            {
+                Console.Clear();
+                Console.WriteLine("   0   1   2   3   4   5   6   7");
+                for (int i = 0; i < SIZE; i++)
+                {
+                    printDivider();
+
+                    Console.Write(i + "| ");
+
+                    for (int j = 0; j < SIZE; j++)
+                    {
+                        Console.Write(board[i][j] + " | ");
+
+                    }
+
+                    Console.WriteLine();
+                }
+                printDivider();
+            }
+
+            void promtUser(string axis)
+            {
+                Console.WriteLine("Enter a " + axis + " coordinate between 0 and 7: ");
+            }
+
+            string pickPiece()
+            {
+                promtUser("x");
+                int coordinateX = int.Parse(Console.ReadLine());
+                promtUser("y");
+                int coordinateY = int.Parse(Console.ReadLine());
+
+                return board[coordinateY][coordinateX] = " ";
+            }
+
+            string pickDestination()
+            {
+                Console.WriteLine("Pick a location to move your piece. ");
+                promtUser("x");
+                int newcoordinateX = int.Parse(Console.ReadLine());
+                promtUser("y");
+                int newcoordinateY = int.Parse(Console.ReadLine());
+
+                return board[newcoordinateY][newcoordinateX] = "X";
+            }
+
+            void movePiece()
+            {
+                pickPiece();
+                pickDestination();
+                PrintBoard();
+            }
+
+         }
     }
 }
